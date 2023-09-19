a2ad9506b5a55440ca4d52eff88479670b1a8724
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2023-09-19 06:52:09 +0530

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,33 @@
+public class sqaure {
+
+    public static void main(String[] args) {
+        System.out.println(perfect_sqaure(189, 3));
+    }
+
+    static double perfect_sqaure(int n,int p){
+        int start =1;
+        int end =n;
+        double root =0.0;
+        while(start<=end){
+            int mid = start+ (end - start   )/2;
+            if(mid*mid==n)return mid;
+            if(mid*mid > n){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+
+         double incr =0.1;
+        for(int i=0;i<p;i++){
+            while(root*root<=n){
+                root+=incr;
+            }
+            root-=incr;
+            incr/=10;
+        }
+      return root;
+
+    }
+}
